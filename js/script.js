e27c9cf36ac84b31ac8458648139ac191d85e303
Danil <danilcka2234@gmail.com>
2020-11-13 00:38:46 +0200

    fix

--- js/script.js
+++ js/script.js
@@ -1,32 +1,56 @@
-// мин, макс, сумма
-const masMax = arr => {
+// max
+const arrayMax = arr => {
     let max
-    let min
-    let sum = 0
     for (let i = 0; i < arr.length; i++) {
         if (typeof arr[i] === 'number' && isNaN(arr[i]) === false) {
-            if (typeof(min, max) === 'undefined') {
-                min = arr[i]
+            if (typeof(max) === 'undefined') {
                 max = arr[i]
             }
 
             if (arr[i] > max) {
                 max = arr[i]
             }
+        }
+    }
+    return max
+}
+arrayMax([NaN, 1, undefined, 3, 5, -3])
+
+//min
+
+const arrayMin = arr => {
+    let min
+    for (let i = 0; i < arr.length; i++) {
+        if (typeof arr[i] === 'number' && isNaN(arr[i]) === false) {
+            if (typeof(min) === 'undefined') {
+                min = arr[i]
+            }
 
             if (arr[i] < min) {
                 min = arr[i]
             }
+        }
+    }
+    return min
+}
+arrayMin([NaN, 1, undefined, 3, 5, -3])
+
+//sum
+
+const arraySum = arr => {
+    let sum = 0
+    for (let i = 0; i < arr.length; i++) {
+        if (typeof arr[i] === 'number' && isNaN(arr[i]) === false) {
             sum += arr[i]
         }
     }
-    console.log('Максимальное: ' + max + '  Минимальное: ' + min + '  Сумма: ' + sum)
+    console.log(sum)
 }
-masMax([NaN, 1, undefined, 3, 5, -3])
+arraySum([NaN, 1, undefined, 3, 5, -3])
 
-// задача с ямками
+// tast 2
 
-const some = array => {
+const getWaterAmong = array => {
     let water = 0
     let first = 0
     let second = first + 2
@@ -50,6 +74,6 @@ const some = array => {
             }
         }
     }
-    console.log(water)
+    return water
 }
 some([0, 1, 1, 3, 1, 5, 0, 3, 4, 7, 7, 3, 3, 0])
